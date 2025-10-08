6c6671b4ecbf1a8c1650b13d5bff808a539a4711
Felipe Rafael Barbosa <rafaelt.ibarbosa@gmail.com>
2025-10-08 13:25:48 -0300

    done

--- main.go
+++ main.go
@@ -4,17 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	auth "github.com/abbot/go-http-auth"
 )
+
+func Secret(user, realm string) string {
+	if user == "felipe" {
+		// password is "12345"
+		return "$1$LEQnFZBF$0OGKwyCrxIzqJBw8qd72e0"
+	}
+	return ""
+}
 
 func main() {
 
+	if len(os.Args) != 3 {
+		fmt.Println("Please check your arguments.")
+		os.Exit(1)
+	}
+
+	httpDir := "/home/felipe/tmp"
 	port := "8080"
-
-
-	fs := http.FileServer(http.Dir("/home/felipe/devops/tmp"))
+	authenticator := auth.NewBasicAuthenticator("example.com", Secret)
 
 	fmt.Println("::::: Server started in: http://localhost:"+port, ":::: 🚀")
 
-	log.Fatal(http.ListenAndServe(":"+port, fs))
+	http.HandleFunc("/", authenticator.Wrap(func(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
+		fmt.Fprintf(w, "<html><body><h1>Hello, %s!</h1></body></html>", r.Username)
+		http.FileServer(http.Dir(httpDir)).ServeHTTP(w, &r.Request)
+	}))
 
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
